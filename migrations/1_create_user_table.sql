eb7de0c51a369b5542fa3b3c20fdc10c89c45a07
santhoshlaxman2002 <santhoshlaxman2020@gmail.com>
2025-10-05 16:39:26 +0530

    feat : Updated the user schema

--- migrations/1_create_user_table.sql
+++ migrations/1_create_user_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  full_name VARCHAR(100) NOT NULL,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
   username VARCHAR(50) UNIQUE NOT NULL,
   email VARCHAR(100) UNIQUE NOT NULL,
   password_hash VARCHAR(255) NOT NULL,
